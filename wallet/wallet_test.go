d261cc8028967dc89bdc87e0701bcce1998c6cd0
Jan Bormet <jan.bormet@bormet.info>
2023-04-12 15:14:23 +0200

    wallet: Add go-perun's generic wallet tests

---
+++ wallet/wallet_test.go
@@ -0,0 +1,59 @@
+package wallet_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"math/rand"
+	gptest "perun.network/go-perun/wallet/test"
+	"perun.network/perun-ckb-backend/wallet"
+	pkgtest "polycry.pt/poly-go/test"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	w := wallet.NewWallet()
+
+	acc, err := w.AddNewAccount()
+	require.NoError(t, err)
+	msg := []byte("hello world")
+	sig, err := acc.SignData(msg)
+	require.NoError(t, err)
+	valid, err := wallet.Backend.VerifySignature(msg, sig, acc.Address())
+	require.NoError(t, err)
+	require.True(t, valid)
+}
+
+func setup(rng *rand.Rand) *gptest.Setup {
+	w := wallet.NewWallet()
+	acc, err := w.AddNewAccount()
+	if err != nil {
+		panic(err)
+	}
+	acc2, err := wallet.NewAccount()
+	if err != nil {
+		panic(err)
+	}
+	binAddr2, err := acc2.Address().MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+	return &gptest.Setup{
+		Backend:           wallet.Backend,
+		Wallet:            w,
+		AddressInWallet:   acc.Address(),
+		ZeroAddress:       wallet.GetZeroAddress(),
+		DataToSign:        []byte("pls sign me"),
+		AddressMarshalled: binAddr2,
+	}
+}
+
+func TestAddress(t *testing.T) {
+	gptest.TestAddress(t, setup(pkgtest.Prng(t)))
+}
+
+func TestGenericSignatureSizeTest(t *testing.T) {
+	gptest.GenericSignatureSizeTest(t, setup(pkgtest.Prng(t)))
+}
+
+func TestAccountWithWalletAndBackend(t *testing.T) {
+	gptest.TestAccountWithWalletAndBackend(t, setup(pkgtest.Prng(t)))
+}
