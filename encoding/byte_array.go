f4d54771f796b1fc5c90dacaaa4e4e859afa1350
janbormet <jan.bormet@bormet.info>
2023-05-03 16:08:44 +0200

    encoding: Add and move some functionality

--- encoding/byte_array.go
+++
@@ -1,7 +0,0 @@
-package encoding
-
-import "github.com/nervosnetwork/ckb-sdk-go/v2/types/molecule"
-
-func PackByte32(b [32]byte) *molecule.Byte32 {
-	return molecule.Byte32FromSliceUnchecked(b[:])
-}
---
+++ encoding/molecule.go
@@ -0,0 +1,23 @@
+package encoding
+
+import (
+	"encoding/binary"
+	"errors"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/types/molecule"
+)
+
+func PackByte32(b [32]byte) *molecule.Byte32 {
+	return molecule.Byte32FromSliceUnchecked(b[:])
+}
+
+func UnpackUint64(x *molecule.Uint64) uint64 {
+	return binary.LittleEndian.Uint64(x.AsSlice())
+}
+
+func ToHashType(b *molecule.Byte) (types.ScriptHashType, error) {
+	if b == nil {
+		return "", errors.New("nil byte")
+	}
+	return types.DeserializeHashTypeByte(b[0])
+}
