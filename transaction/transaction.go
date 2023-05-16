fc740fde027c15073e33854dab0b27a38a0cac54
Norbert Dzikowski <norbert@perun.network>
2023-05-16 15:20:58 +0200

    transaction: Make sure to set the recipient for change in PerunTxBuilder

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,6 +1,9 @@
 package transaction
 
 import (
+	"fmt"
+
+	"github.com/nervosnetwork/ckb-sdk-go/v2/address"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/collector"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/collector/builder"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
@@ -10,10 +13,16 @@ type PerunTransactionBuilder struct {
 	*builder.CkbTransactionBuilder
 }
 
-func NewPerunTransactionBuilder(network types.Network, iterator collector.CellIterator, psh *PerunScriptHandler) *PerunTransactionBuilder {
+func NewPerunTransactionBuilder(network types.Network, iterator collector.CellIterator, psh *PerunScriptHandler, sender address.Address) (*PerunTransactionBuilder, error) {
 	b := &PerunTransactionBuilder{
 		CkbTransactionBuilder: builder.NewCkbTransactionBuilder(network, iterator),
 	}
 	b.Register(psh)
-	return b
+
+	encodedAddr, err := sender.Encode()
+	if err != nil {
+		return nil, fmt.Errorf("encoding address: %w", err)
+	}
+	b.AddChangeOutputByAddress(encodedAddr)
+	return b, nil
 }
