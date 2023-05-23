dd835627efbfdcaf1ccfc25df3af87a36f79a096
Norbert Dzikowski <norbert@perun.network>
2023-05-23 11:09:51 +0200

    backend: Give the signer a ckbaddress.Address

--- backend/signer.go
+++ backend/signer.go
@@ -2,6 +2,7 @@ package backend
 
 import (
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/nervosnetwork/ckb-sdk-go/v2/address"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/transaction"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/transaction/signer"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
@@ -10,19 +11,22 @@ import (
 // Signer is the signer used by the backend implementation.
 type Signer struct {
 	key      secp256k1.PrivateKey
+	Address  address.Address
 	TxSigner signer.TransactionSigner
 }
 
-func NewSigner(key secp256k1.PrivateKey) *Signer {
+func NewSigner(addr address.Address, key secp256k1.PrivateKey) *Signer {
 	return &Signer{
 		key:      key,
+		Address:  addr,
 		TxSigner: *signer.NewTransactionSigner(),
 	}
 }
 
-func NewSignerInstance(key secp256k1.PrivateKey, network types.Network) *Signer {
+func NewSignerInstance(addr address.Address, key secp256k1.PrivateKey, network types.Network) *Signer {
 	return &Signer{
 		key:      key,
+		Address:  addr,
 		TxSigner: *signer.GetTransactionSignerInstance(network),
 	}
 }
