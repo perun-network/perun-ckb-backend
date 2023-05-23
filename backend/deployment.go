f914a1f14215c980a6ce660d16bc1f30264f15be
Norbert Dzikowski <norbert@perun.network>
2023-05-23 10:30:38 +0200

    backend: Implement a Deployment abstraction

---
+++ backend/deployment.go
@@ -0,0 +1,40 @@
+package backend
+
+import (
+	"context"
+
+	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
+)
+
+// Deployment contains all information about the deployed scripts necessary to
+// use the go-perun framework. This includes the Perun scripts on their
+// respective networks.
+type Deployment struct {
+	Network types.Network
+
+	PCTSDep types.CellDep
+	PCLSDep types.CellDep
+	PFLSDep types.CellDep
+
+	PCTSCodeHash types.Hash
+	PCTSHashType types.ScriptHashType
+
+	PCLSCodeHash types.Hash
+	PCLSHashType types.ScriptHashType
+
+	PFLSCodeHash types.Hash
+	PFLSHashType types.ScriptHashType
+
+	DefaultLockScript    types.Script
+	DefaultLockScriptDep types.CellDep
+}
+
+type DeploymentConfig struct {
+	DefaultLockScript types.Script
+}
+
+// MkDeployment deploys the Perun scripts on the given network using the given
+// deployment configuration.
+func MkDeployment(ctx context.Context, client Transactor, network types.Network, config DeploymentConfig) (*Deployment, error) {
+	panic("not implemented")
+}
