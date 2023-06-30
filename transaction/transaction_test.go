16b499d28edf527e5186c578c1215b83dfd0aaa4
Norbert Dzikowski <norbert@perun.network>
2023-06-30 12:05:12 +0200

    transaction: Change TransactionBuilder Open test to Balancing test

--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -52,7 +52,7 @@ func TestScriptHandler(t *testing.T) {
 		return mockIterator
 	}
 
-	t.Run("Open", func(t *testing.T) {
+	t.Run("Balancing", func(t *testing.T) {
 		mockIterator := mkMockIterator()
 		iters := map[types.Hash]collector.CellIterator{
 			zeroHash: mockIterator,
@@ -112,30 +112,3 @@ func TestScriptHandler(t *testing.T) {
 	})
 
 }
-
-// Example output:
-// *github.com/nervosnetwork/ckb-sdk-go/v2/types.Transaction {
-//		Version: 0, Hash: github.com/nervosnetwork/ckb-sdk-go/v2/types.Hash [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-//    CellDeps: []*github.com/nervosnetwork/ckb-sdk-go/v2/types.CellDep len: 1, cap: 1, [
-//              *(*"github.com/nervosnetwork/ckb-sdk-go/v2/types.CellDep")(0xc00013e360),
-//    ],
-//    HeaderDeps: []github.com/nervosnetwork/ckb-sdk-go/v2/types.Hash len: 0, cap: 0, nil,
-//    Inputs: []*github.com/nervosnetwork/ckb-sdk-go/v2/types.CellInput len: 2, cap: 2, [
-//              *(*"github.com/nervosnetwork/ckb-sdk-go/v2/types.CellInput")(0xc00018a3b0),
-//							*(*"github.com/nervosnetwork/ckb-sdk-go/v2/types.CellInput")(0xc00018a3c0),
-//    ],
-//    Outputs: []*github.com/nervosnetwork/ckb-sdk-go/v2/types.CellOutput len: 3, cap: 4, [
-//              *(*"github.com/nervosnetwork/ckb-sdk-go/v2/types.CellOutput")(0xc000012768),
-//							*(*"github.com/nervosnetwork/ckb-sdk-go/v2/types.CellOutput")(0xc000013470),
-//    					*(*"github.com/nervosnetwork/ckb-sdk-go/v2/types.CellOutput")(0xc000013680),
-//    ],
-//    OutputsData: [][]uint8 len: 3, cap: 4, [
-//              [],
-//              [0,0,0,0],
-//              [127,0,0,0,127,0,0,0,20,0,0,0,101,0,0,0,106,0,0,0,122,0,0,0,81,0,0,0,20,0,0,0,52,0,0,0,68,0,0,0,76,0,0,0,90,86,116,59,198,53,20,55,241,48,110,209,59,70,224,83,24,195,74,108,...+67 more],
-//    ],
-//    Witnesses: [][]uint8 len: 2, cap: 2, [
-//              [],
-//              [],
-//    ],
-// }
