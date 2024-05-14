35bdf3afa1667a29f87be6c4486f755d723ca405
ravi0131 <ravi.kumar@stud.tu-darmstadt.de>
2024-05-14 16:14:11 +0200

    Marshalling fix
    Fixed Unmarshalling for assets

--- channel/asset/test/randomizer.go
+++ channel/asset/test/randomizer.go
@@ -5,14 +5,27 @@ import (
 
 	"perun.network/go-perun/channel"
 	"perun.network/go-perun/channel/test"
+	btest "perun.network/perun-ckb-backend/backend/test"
 	"perun.network/perun-ckb-backend/channel/asset"
 )
 
 type Randomizer struct{}
 
-func (*Randomizer) NewRandomAsset(*rand.Rand) channel.Asset {
+func (*Randomizer) NewRandomAsset(rng *rand.Rand) channel.Asset {
 	// TODO: Also allow to generate random SUDT asset.
-	return asset.NewCKBytesAsset()
+	randomScript := btest.NewRandomScript(rng)
+	assetSUDT := asset.NewSUDTAsset(&asset.SUDT{
+		TypeScript:  *randomScript,
+		MaxCapacity: 1000,
+	})
+	assetCKbyte := asset.NewCKBytesAsset()
+	var chosenAsset asset.Asset
+	if rng.Intn(2) == 0 {
+		chosenAsset = *assetSUDT
+	} else {
+		chosenAsset = *assetCKbyte
+	}
+	return &chosenAsset
 }
 
 var _ test.Randomizer = (*Randomizer)(nil)
