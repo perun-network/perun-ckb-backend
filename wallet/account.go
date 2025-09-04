bff35537495dadae7c97d3f5ed16c287d18970e2
Ilja von Hoessle <ilja@perun.network>
2025-09-04 16:01:47 +0200

    * Feat(wallet): Use ethereum/go/crypto for signature generation and verification.
    * Test(wallet): Include TestDecredAndGethSignatures, verifying that ECDSA signatures used from the decred SDK and the go-ethereum SDK are equivalent.
    
    Signed-off-by: Ilja von Hoessle <ilja@perun.network>

--- wallet/account.go
+++ wallet/account.go
@@ -1,9 +1,13 @@
 package wallet
 
 import (
+	"crypto/ecdsa"
+	"encoding/asn1"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
-	"github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
+	"github.com/pkg/errors"
+	"math/big"
 	"perun.network/go-perun/wallet"
 	"perun.network/perun-ckb-backend/wallet/address"
 )
@@ -12,6 +16,10 @@ type Account struct {
 	key           *secp256k1.PrivateKey
 	codeHash      types.Hash
 	defaultScript bool
+}
+
+type ECDSASignature struct {
+	R, S *big.Int
 }
 
 // Address returns an address.Participant with the public key belonging to this account and the default payment and
@@ -31,9 +39,33 @@ func (a Account) Address() wallet.Address {
 	return addr
 }
 
+// func (a Account) SignDataOld(data []byte) ([]byte, error) {
+// 	hash := PrefixedHash(data)
+// 	return PadDEREncodedSignature(decredECDSA.Sign(a.key, hash[:]).Serialize())
+// }
+
 func (a Account) SignData(data []byte) ([]byte, error) {
-	hash := PrefixedHash(data)
-	return PadDEREncodedSignature(ecdsa.Sign(a.key, hash[:]).Serialize())
+	phash := PrefixedHash(data)
+
+	// Convert Decred key to standard *ecdsa.PrivateKey for go-ethereum usage
+	ecdsaKey := ConvertDecredKeyToECDSA(a.key)
+
+	// Sign using go-ethereum crypto.Sign, produces 65-byte [R||S||V]
+	sig, err := crypto.Sign(phash, ecdsaKey)
+	if err != nil {
+		return nil, errors.Wrap(err, "sign hash failed")
+	}
+
+	// Extract R and S to marshal DER signature (drop recovery id V)
+	r := new(big.Int).SetBytes(sig[:32])
+	s := new(big.Int).SetBytes(sig[32:64])
+
+	derEncoded, err := asn1.Marshal(ECDSASignature{R: r, S: s})
+	if err != nil {
+		return nil, errors.Wrap(err, "DER marshal failed")
+	}
+
+	return PadDEREncodedSignature(derEncoded)
 }
 
 func NewAccount() (*Account, error) {
@@ -46,4 +78,16 @@ func NewAccount() (*Account, error) {
 
 func NewAccountFromPrivateKey(key *secp256k1.PrivateKey, codeHash types.Hash, defaultScript bool) *Account {
 	return &Account{key: key, codeHash: codeHash, defaultScript: defaultScript}
+}
+
+// ConvertDecredKeyToECDSA converts a decred secp256k1 PrivateKey to an ecdsa.PrivateKey compatible with go-ethereum.
+func ConvertDecredKeyToECDSA(decredKey *secp256k1.PrivateKey) *ecdsa.PrivateKey {
+	ecPriv := new(ecdsa.PrivateKey)
+	ecPriv.PublicKey.Curve = secp256k1.S256()
+	ecPriv.D = new(big.Int).SetBytes(decredKey.Serialize())
+
+	pub := decredKey.PubKey()
+	ecPriv.PublicKey.X = pub.X()
+	ecPriv.PublicKey.Y = pub.Y()
+	return ecPriv
 }
