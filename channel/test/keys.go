cf2ed3be9b449c61f922baa20260269b6839acdf
Sophia Koehler <sophia@perun.network>
2025-09-16 10:35:00 +0200

    fix: submodule init feat: create accounts automatically fix: add go scripts to .yaml

--- channel/test/keys.go
+++ channel/test/keys.go
@@ -3,14 +3,33 @@ package test
 import (
 	"encoding/hex"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
 
 func GetKey(path string) (*secp256k1.PrivateKey, error) {
-
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		// Generate a new private key
+		if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+			return nil, err
+		}
+		privKey, err := secp256k1.GeneratePrivateKey()
+		if err != nil {
+			return nil, err
+		}
+		// Write it to the file in hex format
+		hexKey := hex.EncodeToString(privKey.Serialize())
+		err = os.WriteFile(path, []byte(hexKey+"\n"), 0600)
+		if err != nil {
+			return nil, err
+		}
+		log.Println("Generated new private key and saved to", path)
+		return privKey, nil
+	}
 	keyFile, err := os.Open(path)
 	if err != nil {
 		return nil, err
